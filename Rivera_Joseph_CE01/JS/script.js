818b18f46386fe4ecdd972c5c8f4a6c97ee4ebe6
Joseph D Rivera <josephdrivera15@gmail.com>
2022-01-31 13:54:50 -0500

    Finished

--- Rivera_Joseph_CE01/JS/script.js
+++ Rivera_Joseph_CE01/JS/script.js
@@ -1,4 +1,5 @@
-alert('This is a javascript alert');
-alert('This is a javascript alert 2');
+// this is a pop-up alert and I will come up  you load the page:
+alert('My JavaScript file is connected properly!');
 
-console.log('This is a javascript console log');
+// This message will pop up in the console browser:
+console.log('I can display text in the Console!');
