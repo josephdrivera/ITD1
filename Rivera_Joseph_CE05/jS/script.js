26743406930bbdede053619e90dd50025236092a
Joseph D Rivera <josephdrivera15@gmail.com>
2022-02-19 15:19:07 -0500

    First push

---
+++ Rivera_Joseph_CE05/jS/script.js
@@ -0,0 +1,31 @@
+//alert("is this working?");
+
+/*
+Problem #1 – Discount Double-Check
+In this problem, you will be prompting the user for the price of 2 items bought at a store and give them a discount based on their total.
+
+
+Create an h1 header for this section. Remember you can use if you need a break on your page to help the overall look.
+
+You are going to purchase (2) items from an online store.
+If you spend $100 or more, you will get a 10% discount on your total purchase.
+If you spend between $50 and $100, you will get a 5% discount on your total purchase.
+If you spend less than $50, you will get no discount.
+
+You should prompt the user for the values using the prompt command.
+
+Final results must be in an HTML element.
+
+If the user DOES get a discount make the background of the results Green.
+
+If the user DOES NOT get a discount make the background of the results Yellow.
+
+User Inputs:
+Cost of First Item (in $)
+Cost of Second Item (in $)
+Result To Print Out:
+If they get no discount:  “Your total purchase is $X.”
+Where X is the total of the 2 items. Make sure to round the cost to 2 decimal places!
+Or if they do get a discount: “Your total purchase is $X, which includes your Y% discount.”
+Where X is the total of the 2 items. Make sure to round the cost to 2 decimal places!
+Where Y is the discount they receive. */
