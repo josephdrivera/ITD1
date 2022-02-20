5f70dd3ec4e5a2ef37d413a3c4958d416d3b39ee
Joseph D Rivera <josephdrivera15@gmail.com>
2022-02-20 15:18:57 -0500

    done

--- Rivera_Joseph_CE06/JS/scrip.js
+++ Rivera_Joseph_CE06/JS/scrip.js
@@ -28,3 +28,38 @@ document.querySelector("#convertGal").addEventListener("click", function (e) {
         totalCups.value = cupsAmt; //display in field
     }
 }); //end of convertGallon function
+
+//Movie Start
+document.querySelector("#Ranking").addEventListener("click", function (e) {
+    //when button is clicked
+    if (
+        document.querySelector("#numDvds").reportValidity() &&
+        document.querySelector("#numBlurays").reportValidity() &&
+        document.querySelector("#numVvs").reportValidity()
+    ) {
+        let numDvds = parseInt(document.querySelector("#numDvds").value);
+        let numBlurays = parseInt(document.querySelector("#numBlurays").value);
+        let numUvs = parseInt(document.querySelector("#numVvs").value);
+
+        let totalMovies = numDvds + numBlurays + numUvs;
+        if (totalMovies < 50) {
+            //if less than 50
+            document.querySelector(
+                "#Results"
+            ).innerHTML = `Your collection contains  ${totalMovies} you are a bronze star member!`;
+            document.querySelector("#bronze").src = "IMG/bronzeStar.jpg"; //display bronze star
+        } else if (totalMovies >= 50 && totalMovies <= 100) {
+            //if between 50 and 100
+            document.querySelector(
+                "#Results"
+            ).innerHTML = `Your collection contains  ${totalMovies} you are a silver star member!`;
+            document.querySelector("#bronze").src = "IMG/silverStar.jpg"; //display silver star
+        } else if (totalMovies > 100) {
+            //if more than 100
+            document.querySelector(
+                "#Results"
+            ).innerHTML = `Your collection contains ${totalMovies}  you are a gold star member!`;
+            document.querySelector("#bronze").src = "IMG/goldStar.jpg"; //display gold star
+        }
+    }
+});
